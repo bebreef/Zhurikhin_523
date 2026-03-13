49e75dc9be349fa5db865c5cec14b042019e5e21
bebreef <zurihinruslan@gmail.com>
2026-03-13 17:24:18 +0300

    готовые все 3 теста

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -7,6 +7,22 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
         public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
         {
             // Arrange
@@ -23,9 +39,24 @@ namespace BankTests
             {
                 // Assert
                 StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
             }
+
+            Assert.Fail("The expected exception was not thrown.");
         }
+        [TestMethod]
+        public void Credit_WithValidPositiveAmount_IncreasesBalance()
+        {
+            double beginningBalance = 11.99;
+            double creditAmount = 5.77;
+            double expected = 17.76;   
 
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
 
+            account.Credit(creditAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Баланс не был корректно увеличен после пополнения");
+        }
     }
 }
