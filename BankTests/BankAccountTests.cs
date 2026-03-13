38edc0201f048f0ebe9089b186bad79686b91ef6
bebreef <zurihinruslan@gmail.com>
2026-03-13 16:58:49 +0300

    Проверяем также случай, когда размер списания превышает баланс

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -23,11 +23,11 @@ namespace BankTests
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
         [TestMethod]
-        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
         {
             // Arrange
             double beginningBalance = 11.99;
-            double debitAmount = -100.00;
+            double debitAmount = 100.00;
             BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
 
             // Act and assert
