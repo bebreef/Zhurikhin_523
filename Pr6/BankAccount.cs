0d5c2966150c1d249879b5c26890b7226622c505
bebreef <zurihinruslan@gmail.com>
2026-03-13 16:39:15 +0300

    Написал комментарии и исправил логику вычисления в debit'е

--- Pr6/BankAccount.cs
+++ Pr6/BankAccount.cs
@@ -3,31 +3,56 @@
 namespace BankAccountNS
 {
     /// <summary>
-    /// Bank account demo class.
+    /// Класс представляет собой банковский счёт клиента
+    /// Содержит информацию о владельце счёта и текущем балансе, а также методы для пополнения и снятия денежных средств
     /// </summary>
     public class BankAccount
     {
         private readonly string m_customerName;
         private double m_balance;
-
+       
+        /// <summary>
+        /// Закрытый конструктор по умолчанию, запрещающий создание объекта без указания данных
+        /// </summary>
         private BankAccount() { }
 
+        /// <summary>
+        /// Инициализирует новый экземпляр банковского счёта с указанием имени владельца и начального баланса
+        /// </summary>
+        /// <param name="customerName">ФИО или название владельца счёта</param>
+        /// <param name="balance">Начальная сумма на счёте (может быть 0)</param>
+        /// <exception cref="ArgumentNullException">Вбрасывается, если customerName = null</exception>
         public BankAccount(string customerName, double balance)
         {
             m_customerName = customerName;
             m_balance = balance;
         }
 
+        /// <summary>
+        /// Получает имя владельца банковского счёта
+        /// </summary>
         public string CustomerName
         {
             get { return m_customerName; }
         }
 
+        /// <summary>
+        /// Получает текущий баланс на счёте
+        /// </summary>
         public double Balance
         {
             get { return m_balance; }
         }
 
+        /// <summary>
+        /// Снимает указанную сумму со счёта
+        /// </summary>
+        /// <param name="amount">Сумма, которую необходимо снять со счёта</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Вбрасывается, если:
+        /// • amount меньше нуля
+        /// • amount больше текущего баланса - недостаточно средств
+        /// </exception>
         public void Debit(double amount)
         {
             if (amount > m_balance)
@@ -40,9 +65,16 @@ namespace BankAccountNS
                 throw new ArgumentOutOfRangeException("amount");
             }
 
-            m_balance += amount;
+            m_balance -= amount;
         }
 
+        /// <summary>
+        /// Пополняет счёт на указанную сумму
+        /// </summary>
+        /// <param name="amount">Сумма пополнения</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Выбрасывается, если amount меньше нуля
+        /// </exception>
         public void Credit(double amount)
         {
             if (amount < 0)
@@ -53,6 +85,10 @@ namespace BankAccountNS
             m_balance += amount;
         }
 
+        /// <summary>
+        /// Точка входа в консольное приложение
+        /// Создаёт тестовый счёт, выполняет несколько операций и выводит итоговый баланс
+        /// </summary>
         public static void Main()
         {
             BankAccount ba = new BankAccount("Mr. Roman Abramovich", 11.99);
