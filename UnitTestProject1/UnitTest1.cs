78d95225addf0d72ba8d1a4c029acc62e4f934d0
bebreef <zurihinruslan@gmail.com>
2026-03-25 13:18:18 +0300

    написал пробный тест, дальше рефакторинг

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pr14.Pages;
 
 namespace UnitTestProject1
 {
@@ -7,8 +8,13 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AuthTest()
         {
+            var page = new LoginPage();
+            Assert.IsTrue(page.Auth("test", "test1"));
+            Assert.IsFalse(page.Auth("user1", "12345"));
+            Assert.IsFalse(page.Auth("", ""));
+            Assert.IsFalse(page.Auth(" ", " "));
         }
     }
 }
