bf4e2a25f67ece76ed1e11ea3f32397b4d24fe9c
bebreef <zurihinruslan@gmail.com>
2026-04-20 13:44:57 +0300

    добавил комментарии

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fibonacci
 {
@@ -11,24 +7,37 @@ namespace Fibonacci
         static void Main(string[] args)
         {
             int result = Fibonacci(5);
-            Console.WriteLine(result);
+            Console.WriteLine($"Результат: {result}");
+        }
 
-            int Fibonacci(int n)
+        /// <summary>
+        /// Вычисляет n-е число Фибоначчи (рекурсивная версия с итеративной реализацией внутри).
+        /// </summary>
+        /// <param name="n">Номер числа Фибоначчи (n ≥ 0)</param>
+        /// <returns>n-е число последовательности Фибоначчи</returns>
+        /// <remarks>
+        /// Последовательность: 0, 1, 1, 2, 3, 5, 8, 13, ...
+        /// Для n=0 возвращает 0, для n=1 возвращает 1.
+        /// </remarks>
+        static int Fibonacci(int n)
+        {
+            Console.WriteLine("The output is: ");
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i <= n; i++)
             {
-                Console.WriteLine("The output is: ");
-                int n1 = 0;
-                int n2 = 1;
-                int sum;
-
-                for (int i = 2; i <= n; i++)
-                {
-                    sum = n1 + n2;
-                    n1 = n2;
-                    n2 = sum;
-                }
-
-                return n == 0 ? n1 : n2;
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
             }
+
+            return n2;
         }
     }
 }
