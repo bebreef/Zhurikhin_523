d171ad8dfda8c932c975ab6641fc6d5d6a0963ae
bebreef <zurihinruslan@gmail.com>
2026-04-23 13:36:13 +0300

    Заполнен файл с тестами, сделана usecase диаграмма и реализованы автотесты ДО создания самого приложения

---
+++ UnitTestProject5/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject5")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61346f55-7f0f-4b5e-b4a3-0dbf774b2e40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject5/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UnitTestProject5
+{
+    [TestClass]
+    public class RsaCipherTests
+    {
+        [TestMethod]
+        [DataRow("Hello", 17, 19)]
+        [DataRow("TestRSA", 23, 29)]
+        public void EncryptDecrypt_LatinText_RoundtripSuccessful(string originalText, int p, int q)
+        {
+            var (e, d, n) = RsaCipher.GenerateKeys(p, q);
+
+            List<BigInteger> encrypted = RsaCipher.Encrypt(originalText, e, n);
+            string decrypted = RsaCipher.Decrypt(encrypted, d, n);
+
+            Assert.AreEqual(originalText, decrypted, "Дешифрованный текст не совпадает с исходным.");
+            Assert.IsTrue(encrypted.Any(c => c != (BigInteger)(int)originalText[0]),
+                "Зашифрованные данные должны отличаться от исходных.");
+        }
+        [TestMethod]
+        [DataRow("Привет", 257, 263)]
+        [DataRow("Шифрование", 311, 313)]
+        public void EncryptDecrypt_CyrillicText_WithBigInteger(string originalText, int p, int q)
+        {
+            var (e, d, n) = RsaCipher.GenerateKeys(p, q);
+
+            List<BigInteger> encrypted = RsaCipher.Encrypt(originalText, e, n);
+            string decrypted = RsaCipher.Decrypt(encrypted, d, n);
+
+            Assert.AreEqual(originalText, decrypted, "Потеря данных при шифровании Unicode-символов.");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Encrypt_Throws_WhenModulusTooSmall()
+        {
+            char symbol = 'Я';
+            var (e, d, n) = RsaCipher.GenerateKeys(7, 11);
+
+            RsaCipher.Encrypt(symbol.ToString(), e, n);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GenerateKeys_Throws_OnCompositeNumber()
+        {
+            RsaCipher.GenerateKeys(15, 13);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Encrypt_Throws_OnNullInput()
+        {
+            var (e, d, n) = RsaCipher.GenerateKeys(11, 13);
+            RsaCipher.Encrypt(null, e, n);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Encrypt_Throws_OnEmptyInput()
+        {
+            var (e, d, n) = RsaCipher.GenerateKeys(11, 13);
+            RsaCipher.Encrypt("", e, n);
+        }
+    }
+}
